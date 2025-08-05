4c306d13451bfeca110369ddf5aa02934c9c764c
« Ephely » <ephely1@gmail.com>
2025-08-05 16:26:17 +0200

    Added Task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Write a function def cat_arrays(arr1, arr2):
+that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """def cat_arrays"""
+    result = arr1 + arr2
+    return result
