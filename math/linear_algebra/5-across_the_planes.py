14542908250d0d40051a0ed9f20954fc30f954cb
« Ephely » <ephely1@gmail.com>
2025-08-05 15:49:41 +0200

    Added Task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Write a function def add_matrices2D(mat1, mat2)
+that adds two matrices element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    new_matrix = []
+    if len(mat1) == len(mat2):
+        for row1, row2 in zip(mat1, mat2):
+            if len(row1) != len(row2):
+                return None
+            new_row = [a + b for a, b in zip(row1, row2)]
+            new_matrix.append(new_row)
+    return new_matrix
