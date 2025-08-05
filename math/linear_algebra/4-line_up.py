bdd3e799081fa48871528229fd4ebf18d40bfc65
« Ephely » <ephely1@gmail.com>
2025-08-05 11:37:41 +0200

    Added Task 4

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Write a function def add_arrays(arr1, arr2)
+that adds two arrays element-wise"""
+
+
+def add_arrays(arr1, arr2):
+    if len(arr1) == len(arr2):
+        new_array=[]
+        for a, b in zip(arr1, arr2):
+            new_array.append(a + b)
+        return new_array
+    return None
