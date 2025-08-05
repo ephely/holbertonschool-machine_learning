46eca1878380a47b294d3e414b7aca9986a65e66
« Ephely » <ephely1@gmail.com>
2025-08-05 10:18:41 +0200

    Added Task 3

--- math/linear_algebra/2-main.py
+++ math/linear_algebra/2-main.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-"""Write a function def matrix_shape(matrix) 
-that calculates the shape of a matrix"""
 
 matrix_shape = __import__('2-size_me_please').matrix_shape
 
--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Write a function def matrix_shape(matrix)
+that calculates the shape of a matrix"""
 def matrix_shape(matrix):
     size = []
     while isinstance(matrix, list):
