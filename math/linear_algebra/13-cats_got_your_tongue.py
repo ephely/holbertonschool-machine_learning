f9cde3acff803b5bc0fb6d011ed4749497896b03
« Ephely » <ephely1@gmail.com>
2025-08-07 15:54:27 +0200

    Fixed Task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -2,7 +2,7 @@
 """"Write a function def np_cat(mat1, mat2, axis=0)
 that concatenates two matrices along a specific axis"""
 import numpy as np
-"""import"""
+"""numpy"""
 
 
 def np_cat(mat1, mat2, axis=0):
