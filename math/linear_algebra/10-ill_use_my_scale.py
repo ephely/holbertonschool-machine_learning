c717c3b259299706525afbc710d4cd43f1503cbe
« Ephely » <ephely1@gmail.com>
2025-08-07 14:43:23 +0200

    Fixed Task 10

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-import numpy as np
 """Write a function def np_shape(matrix)
 that calculates the shape of a numpy.ndarray"""
 
 
 def np_shape(matrix):
     """np_shape"""
-    return np.array(matrix).shape
+    return matrix.shape
