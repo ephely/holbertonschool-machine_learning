77ec68ff1d9ab19138e5374c1b2ae597e72f64c9
« Ephely » <ephely1@gmail.com>
2025-08-07 16:03:16 +0200

    Added Task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""""Write a function def np_matmul(mat1, mat2)
+that performs matrix multiplication"""
+import numpy as np
+"""numpy"""
+
+
+def np_matmul(mat1, mat2):
+    """np_matmul"""
+    return np.matmul(mat1, mat2)
